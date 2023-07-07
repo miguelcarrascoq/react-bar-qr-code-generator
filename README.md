15a2d3cce36b1b29dfeb0e52f9c3a549920ac47e
Miguel Carrasco Q <miguel.carrasco.quijada@gmail.com>
2023-07-06 21:10:37 -0400

    feat(react-qr-code): add react-qr-code
    `npm install react-qr-code`

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # react-bar-qr-code-generator
 
+- barcode https://github.com/kciter/react-barcode
+- qr code https://github.com/rosskhanas/react-qr-code
+
 
--- bar-qr-code/app/page.tsx
+++ bar-qr-code/app/page.tsx
@@ -1,12 +1,17 @@
 'use client'
 
 var Barcode = require('react-barcode');
+import QRCode from "react-qr-code";
 
 export default function Home() {
+
+  const rut = '11.222.333-9';
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <Barcode value="14.133.612-6" displayValue="true" />
+        <Barcode value={rut} displayValue="true" />
+        <QRCode value={rut} />
       </div>
     </main>
   )
